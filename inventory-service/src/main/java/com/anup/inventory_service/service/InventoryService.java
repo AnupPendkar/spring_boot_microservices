619708c8f5819cd5ddf08282cab8cdad7ca7d34f
panup <anup.p@atai.ai>
2024-11-29 17:34:24 +0530

    #Add: inventory service added.

---
+++ inventory-service/src/main/java/com/anup/inventory_service/service/InventoryService.java
@@ -0,0 +1,19 @@
+package com.anup.inventory_service.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.anup.inventory_service.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
+}
